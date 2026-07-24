fe87950599c8dc5a6684ae41c18f682dc95ebb3c
Zela Varginha <speed.varginha@gmail.com>
2026-07-24 12:47:36 -0300

    chore(dados): coleta diaria automatica 24/07/2026 12:47

--- painel-cidadao/emendas/data/emendas_municipais_unificadas.js
+++ painel-cidadao/emendas/data/emendas_municipais_unificadas.js
@@ -1,6 +1,6 @@
 window.EMENDAS_MUNICIPAIS_UNIFICADAS = {
  "metadata": {
-  "geradoEm": "2026-07-23T13:40:44.140736+00:00",
+  "geradoEm": "2026-07-24T15:40:52.250833+00:00",
   "fonte": "Histórico Betha (até 2024) + SAPL/Câmara (2025 em diante)",
   "criterioDeduplicacao": "número/ano da emenda; SAPL é canônico a partir de 2025",
   "totalRegistros": 581,
